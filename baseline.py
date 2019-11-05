9bd5a912aad746ecffa50f4c9ba918fafdb9ff05
guy <gwuollet@stanford.edu>
2019-11-04 18:39:57 -0800

    yea

--- baseline.py
+++ baseline.py
@@ -1,5 +1,6 @@
 import click
 import numpy as np
+import utils
 
 
 @cli.command()
--- utils.py
+++ utils.py
@@ -3,4 +3,14 @@ import pandas as pd
 
 
 def load_data():
-    
+
+    train_data = os.path.join("data", "train.csv")
+
+    df = pd.read_csv(data)
+    X_train = df['id', 'comment_text']
+    y_train = df['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+
+    X_test = os.path.join("data", "test.csv")
+    y_test = os.path.join("data", "test_labels.csv")
+
+    return X_train, y_train, X_test, y_test 
